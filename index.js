81934a41c804fffca85d951755733e670d401c3a
luisdev <redhottemp@gmail.com>
2025-05-08 22:11:02 -0400

    Create pseudocode and code for human choice and update computer choice selection to upper case

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 // Create getComputerChoice function
 function getComputerChoice() {
     // Create a random number 1-2
-    randomNumber = Math.floor(Math.random() * 3);
+    let randomNumber = Math.floor(Math.random() * 3);
         // If random number equals 1 console Rock
         if (randomNumber === 1) {
-            console.log("Rock");
+            console.log("ROCK");
         } else if (randomNumber === 2) {
         // Else if random number equals 2 console Paper
-            console.log("Paper");
+            console.log("PAPER");
         } else {
         // Else console Scissors
-            console.log("Scissors");
+            console.log("SCISSORS");
         }
 // End the function
 }
 
+// Create getHumanChoice function
+function getHumanChoice() {
+    // Prompt the user for Rock, Paper, or Scissors choice
+    let humanInput = prompt("Type your choice of Rock, Paper, or Scissors");
+    // Console the user's input
+    console.log(humanInput.toUpperCase());
+// End the function 
+}
+
 getComputerChoice();
-getComputerChoice();
+getHumanChoice();
