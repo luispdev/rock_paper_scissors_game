cae8ce6d47affde82879a93bcd537afb0c8f5237
luisdev <redhottemp@gmail.com>
2025-05-08 21:42:14 -0400

    Create pseudocode and code for computer choice

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Testing from index.js file!")
+// Create getComputerChoice function
+function getComputerChoice() {
+    // Create a random number 1-2
+    randomNumber = Math.floor(Math.random() * 3);
+        // If random number equals 1 console Rock
+        if (randomNumber === 1) {
+            console.log("Rock");
+        } else if (randomNumber === 2) {
+        // Else if random number equals 2 console Paper
+            console.log("Paper");
+        } else {
+        // Else console Scissors
+            console.log("Scissors");
+        }
+// End the function
+}
+
+getComputerChoice();
+getComputerChoice();
